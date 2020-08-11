d437f734815ff8d95825d0763d645c3d8770b548
Francois de Klerk <francoisdk@bbd.co.za>
2020-08-11 09:41:24 +0200

    init

---
+++ src/test/java/com/face/recognition/RecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.face.recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
