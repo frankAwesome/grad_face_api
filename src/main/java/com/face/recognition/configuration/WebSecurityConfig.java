b0ee17af2e00bf4a9ad7ae2ed1729667ddc30eca
Dylan Carstens <dylanca@bbd.co.za>
2020-08-26 18:48:02 +0200

    authorized swagger

--- src/main/java/com/face/recognition/configuration/WebSecurityConfig.java
+++ src/main/java/com/face/recognition/configuration/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -41,10 +40,27 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
-				.authorizeRequests().antMatchers("/api/v1/register","/api/v1/login").permitAll().
-						anyRequest().authenticated().and().
-						exceptionHandling().and().sessionManagement()
+		httpSecurity.csrf()
+				.disable()
+				.authorizeRequests()
+				.antMatchers(
+				"/api/v1/register",
+						"/api/v1/login",
+						"/v3/api-docs/**",
+						"/swagger-ui.html",
+						"/swagger-ui/**",
+						"/v2/api-docs",
+						"/swagger-resources/configuration/ui",
+						"/swagger-resources",
+						"/swagger-resources/configuration/security",
+						"/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.exceptionHandling()
+				.and()
+				.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
